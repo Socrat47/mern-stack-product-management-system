4263cda69fbee35e8889febeabbd0f77b13388b0
socrat <78218332+Socrat47@users.noreply.github.com>
2024-12-24 20:16:55 +0300

    Remove all files from version control

--- backend/routes/category.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const deneme = require('../controller/category.js');
-
-const router = express.Router();
-
-router.post('/create-category', deneme);
-
-module.exports = router;
